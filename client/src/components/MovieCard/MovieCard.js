a33dc1cc3322e302516814a927533d261afa143c
Raj Vardhan Singh Birani <rbrajvardhan@gmail.com>
2022-04-28 03:39:56 +0530

    feat: created a movie context to pass mocie details inside modals and completed the basic layout of movie modal

--- client/src/components/MovieCard/MovieCard.js
+++ client/src/components/MovieCard/MovieCard.js
@@ -30,13 +30,17 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import MovieModal from "../MovieModal/MovieModal";
 
+export const MovieContext = React.createContext();
+
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  height: 600,
-  width: 600,
+  height: "80vh",
+  width: "60vw",
+  padding: 0,
+  margin: 0,
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
@@ -49,44 +53,47 @@ export default function MovieCard({ movie }) {
   const handleClose = () => setOpen(false);
 
   return (
-    <div>
-      <div className="movie-card" key={movie.id} onClick={handleOpen}>
-        <img
-          className="movie-card__image"
-          src={movie.posterurl}
-          alt={movie.originalTitle}
-        />
-        <div className="movie-card__details">
-          <h2>{movie.title}</h2>
+    <MovieContext.Provider value={movie}>
+      <div>
+        <div className="movie-card" key={movie.id} onClick={handleOpen}>
+          <img
+            className="movie-card__image"
+            src={movie.posterurl}
+            alt={movie.originalTitle}
+          />
+          <div className="movie-card__details">
+            <h2>{movie.title}</h2>
+          </div>
         </div>
-      </div>
 
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <Box sx={style}>
-            <Typography
-              id="transition-modal-title"
-              variant="h6"
-              component={MovieModal}
-            >
-              Text in a modal
-            </Typography>
-            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
-          </Box>
-        </Fade>
-      </Modal>
-    </div>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <Box sx={style}>
+              {/* <Typography
+                id="transition-modal-title"
+                variant="h6"
+                component="h2"
+              >
+                Text in a modal
+              </Typography> */}
+              <MovieModal />
+              {/* <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography> */}
+            </Box>
+          </Fade>
+        </Modal>
+      </div>
+    </MovieContext.Provider>
   );
 }
--- client/src/components/MovieModal/MovieModal.js
+++ client/src/components/MovieModal/MovieModal.js
@@ -1,4 +1,39 @@
+import { useContext } from "react";
+import "./MovieModal.scss";
+import { MovieContext } from "../MovieCard/MovieCard";
+
 function MovieModal() {
-  return <div>Hello im movie modal</div>;
+  const movie = useContext(MovieContext);
+
+  return (
+    <div className="movie-modal">
+      <img
+        className="movie-modal__image"
+        src={movie.posterurl}
+        alt={movie.originalTitle}
+      />
+      <div className="movie-details">
+        <h1>{movie.title}</h1>
+        <p>{movie.contentRating}</p>
+        <p>{movie.year}</p>
+        <p>
+          Genres:{" "}
+          {movie.genres.map((genre) => (
+            <span>{genre}</span>
+          ))}
+        </p>
+        <p>{movie.imdbRating}</p>
+        <p>
+          Genres:{" "}
+          {movie.actors.map((actor) => (
+            <span>{actor}</span>
+          ))}
+        </p>
+        <p>{movie.storyline}</p>
+      </div>
+
+      <button>Update</button>
+    </div>
+  );
 }
 export default MovieModal;
